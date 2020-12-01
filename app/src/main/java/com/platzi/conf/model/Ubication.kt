72d22dd2d709ff2a801bc80fc8b122dfc81b994f
Nelly Fragoso Muñoz <nellyfrgoso1003@gmail.com>
2020-11-30 20:57:09 -0600

    Google maps

--- app/src/main/java/com/platzi/conf/model/Ubication.kt
+++ app/src/main/java/com/platzi/conf/model/Ubication.kt
@@ -1,12 +1,12 @@
 package com.platzi.conf.model
 
 class Ubication {
-    val name = ""
-    val address = ""
-    val latitude = ""
-    val longitude = ""
-    val phone = ""
-    val website = ""
-    val photo = ""
+    val name = "Mi House"
+    val address = "San Jose Tetel, 90454  Tlax."
+    val latitude = 20.4066156
+    val longitude = -95.1700264
+    val phone = "+52 241 135 1034"
+    val webside = "https://elcantoral.com/"
+    val photo = "null"
 
 }
--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -7,6 +7,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.type.LatLng
+
+import com.platzi.conf.model.Ubication
+
 import com.platzi.conf.R
 
 /**
@@ -20,6 +29,29 @@ class UbicationFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ubication, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.navMapFragment) as SupportMapFragment
+
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+
+        val zoom = 16f
+        val centerMap = com.google.android.gms.maps.model.LatLng(ubication.latitude, ubication.longitude)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap,zoom))
+
+        val centerMark = com.google.android.gms.maps.model.LatLng(ubication.latitude, ubication.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(centerMark)
+        markerOptions.title("Jair conf")
+
+
     }
 
 
